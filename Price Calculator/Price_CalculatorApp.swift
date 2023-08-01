29c3b778f155a1b3949a6ad8c1eb2759ac11e371
Michał Gorzkowski <michal@mgorzkowski.pl>
2023-08-01 11:28:55 +0200

    Initial Commit

---
+++ Price Calculator/Price_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Price_CalculatorApp.swift
+//  Price Calculator
+//
+//  Created by Michał Gorzkowski on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Price_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
